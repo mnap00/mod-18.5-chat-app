04c9214cc3d66e45c9f2a4219d02e33cfe3b48e6
mnap <learn@10g.pl>
2018-05-05 22:30:25 +0200

    Add script - send message

--- src/index.js
+++ src/index.js
@@ -37,6 +37,16 @@ io.on('connection', function(socket) {
     });
 });
 
+io.on('connection', function(socket) {
+    socket.on('message', function(message) {
+        const {name} = userService.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text: message.text,
+            from: name
+        });
+    });
+});
+
 server.listen(3000, function() {
     console.log('listening on *:3000');  // eslint-disable-line no-console
 });
