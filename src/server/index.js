f17f78d6a00e437941dc3c14f2535d7638510c71
mnap <learn@10g.pl>
2018-05-06 20:05:17 +0200

    Adjust project structure

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const UsersService = require('./UsersService');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const UsersService = require('./UsersService');
 
 const userService = new UsersService();
 
